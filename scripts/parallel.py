30d7e70ca3f0476b221088a87bffc6227d0f80d0
Zach Zhao <thezachzhao@gmail.com>
2021-01-04 15:09:05 -0500

    Improve logging and add progress bar

--- scripts/parallel.py
+++ scripts/parallel.py
@@ -1,27 +1,43 @@
 import sys
 import os
 import csv
-# Set path to ContactTracing/
-os.chdir('..')
-sys.path.insert(0, '.')
-print(f"Current working directory: {os.getcwd()}")
-print(f"Current path {sys.path}")
+from tqdm import tqdm
+import logging
 
 import pandas as pd
 import itertools
 import time
 import concurrent.futures
+
+# Set path to ContactTracing/
+os.chdir('..')
+sys.path.insert(0, '.')
+
+output_file = f'output/plots/results[{time.strftime("%H-%M-%S")}].csv'
+logging_file = f'output/plots/results[{time.strftime("%H-%M-%S")}].log'
+
+logging.basicConfig(filename=logging_file, level=logging.DEBUG)
+
+logging.info(f"Current working directory: {os.getcwd()}")
+logging.info(f"Current path {sys.path}")
+
 from ctrace.simulation import *
 
 
-def runner(args):
-    """Example parallel function (only accepts one argument)"""
-    a, b = args
-    print(f'Doing {a} and {b}')
-    return a + b
+def minimize_params(p):
+    p = p.copy()
+    # Reduce graph to graph name only
+    p["G"] = p["G"].NAME
 
+    # Remove unnecessary parameters not needed for logging
+    del p["S"]
+    del p["I_t"]
+    del p["R"]
+    del p["visualization"]
+    del p["verbose"]
+    return p
 
-def parallel(func, args, logging=True):
+def parallel(func, args, output_file=None):
     """Parallelize the simulate function"""
     # TODO: Make this function generic
     entries = []
@@ -29,38 +45,39 @@ def parallel(func, args, logging=True):
         start = time.perf_counter()
         results = [executor.submit(func, arg) for arg in args]
 
-        with open('output/plots/logging.csv', "w") as logging_file:
+        if not output_file:
+            output_file = f'output/plots/results.csv'
+        with open(output_file, "w") as logging_file:
+            # Attributes to log to files
             attr = [
-                "G",
+                "G",            # The parameters of the experiment
                 "SIR_file",
                 "budget",
                 "iterations",
                 "method",
-                "trial_id",
-                "num_infected",
+                "p",
+                "trial_id",     # The trial number
+                "num_infected", # Output Parameters
                 "peak",
             ]
             writer = csv.DictWriter(logging_file, fieldnames=attr)
             writer.writeheader()
-            for f in concurrent.futures.as_completed(results):
+            for f in tqdm(concurrent.futures.as_completed(results), total=len(args)):
                 ((infected, peak), param) = f.result()
+                # Reduce parameters to viewable ones only
+                param_view = minimize_params(param)
                 entry = {
-                    "G": param["G"].NAME,
-                    "SIR_file": param["SIR_file"],
-                    "budget": param["budget"],
-                    "iterations": param["iterations"],
-                    "method": param["method"],
-                    "trial_id": param["trial_id"],
+                    **param_view,
                     "num_infected": infected,
                     "peak": peak,
                 }
                 writer.writerow(entry)
                 entries.append(entry)
                 logging_file.flush()
-                print(f"Finished => {entry}")
+                logging.info(f"Finished => {entry}")
 
         finish = time.perf_counter()
-        print(f'Finished in {round(finish - start, 2)} seconds')
+        logging.info(f'Finished in {round(finish - start, 2)} seconds')
     return entries
 
 def dict_product(dicts):
@@ -69,27 +86,28 @@ def dict_product(dicts):
 
 # <========================================== Main ==========================================>
 # Generate arguments
-print(os.getcwd())
 G = load_graph("montgomery")
 SIR_file = "Q4data.json"
 (S, I, R) = initial(from_cache=SIR_file)
-print("Loaded!")
+logging.info("Loaded!")
 
 # <========================================== Important Parameters ==========================================>
+
 # Cartesian Product the parameters
-TRIALS = 2
+TRIALS = 5
 compact_params = {
+    "trial_id": [x for x in range(TRIALS)],
     "G": [G],
-    "budget": [x for x in range(100, 1000, 100)],  # k
+    "budget": [x for x in range(100, 201, 100)],  # k
     "S": [S],
     "I_t": [I],
     "R": [R],
     "SIR_file": [SIR_file],
     "iterations": [-1],
     "method": ["random"],
+    "p": [0.05, 0.1, .15],
     "visualization": [False],
     "verbose": [False],
-    "trial_id": [x for x in range(TRIALS)],
 }
 
 # <========================================== End of Parameters ==========================================>
@@ -99,26 +117,10 @@ params = list(dict_product(compact_params))
 # print(params)
 # Runs multiple trials
 def simulate(param):
-    print(
-        f"Launching => budget: { param['budget'] }, method: {param['method']}, trial: {param['trial_id']}")
+    logging.info(
+        f"Launching => {minimize_params(param)}"
+    )
     (infected, peak) = MDP(**param)
     return ((infected, peak), param)
 
-results = list(parallel(simulate, params))
-
-# # <=========================== Save results into CSV ===========================>
-# rows = []
-# # Save only the graph name and append results
-# for param, (num, peak) in zip(params, results):
-#     rows.append({
-#         "G": param["G"].NAME,
-#         "SIR_file": SIR_file,
-#         "budget": param["budget"],
-#         "iterations": param["iterations"],
-#         "method": param["method"],
-#         "num_infected": num,
-#         "peak": peak,
-#     })
-
-# df = pd.DataFrame(rows)
-# df.to_csv("output/plots/output.csv")
+results = list(parallel(simulate, params, output_file))
