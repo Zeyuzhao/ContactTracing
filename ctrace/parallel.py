9c26fec1fc48677f3137d6b901009e365ca53b58
Zach Zhao <thezachzhao@gmail.com>
2021-01-03 19:28:07 -0500

    Parallel script

--- ctrace/parallel.py
+++ ctrace/parallel.py
@@ -1,8 +1,7 @@
 import concurrent.futures
 import time
 from simulation import *
-
-
+import itertools
 
 def runner(args):
     """Example parallel function (only accepts one argument)"""
@@ -10,16 +9,41 @@ def runner(args):
     print(f'Doing {a} and {b}')
     return a + b
 
-def parallel(func, args):
-    start = time.perf_counter()
+def parallel(func, args, logging=True):
     with concurrent.futures.ProcessPoolExecutor() as executor:
+        start = time.perf_counter()
         results = executor.map(func, args)
-        for result in results:
-            print(result)
-    finish = time.perf_counter()
-    print(f'Finished in {round(finish - start, 2)} seconds')
+        finish = time.perf_counter()
+        if logging:
+            print(f'Finished in {round(finish - start, 2)} seconds')
+        return results
 
+def dict_product(dicts):
+    """Expands an dictionary of lists into a cartesian product of dictionaries"""
+    return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
 
-if __name__ == '__main__':
-    args = []
-    parallel(runner, [(1, 1), (2, 3), (4, 4)])
+# <========================================== Main ==========================================>
+# Generate arguments
+G = load_graph("montgomery")
+(S, I, R) = initial(G)
+
+# Cartesian Product the parameters
+compact_params = {
+    "G": [G],
+    "budget": [x for x in range(100, 1000, 10)],
+    "S": [S],
+    "I_t": [I],
+    "R": [R],
+    "iterations": 15,
+    "method": ["none", "degree", "random", "dependent", "iterated", "optimized"],
+    "visualization": [False],
+    "verbose": [False],
+}
+
+params = dict_product(compact_params)
+
+def simulate(param):
+    return MDP(**param)
+
+results = parallel(simulate, params)
+
