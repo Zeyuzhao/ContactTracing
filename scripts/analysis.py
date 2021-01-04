5eda07217dfc565380317480cfd4b7cef245e886
Zach Zhao <thezachzhao@gmail.com>
2021-01-04 13:48:10 -0500

    Parallel Fix

---
+++ scripts/analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+with open("../output/plots/logging.csv", "r") as csv:
+    df = pd.read_csv(csv)
+print(df)
+random = df.loc[df["method"] == "random"]
+degree = df.loc[df["method"] == "degree"]
+
+print(random)
+print(degree)
