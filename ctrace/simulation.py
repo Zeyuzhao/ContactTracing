0ecbc4e91c14e784542b10f43914963a24065019
Zach Zhao <thezachzhao@gmail.com>
2021-01-05 21:47:57 -0500

    Fix cache capability in simulation.py

--- ctrace/simulation.py
+++ ctrace/simulation.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import random
 import EoN
+from collections import namedtuple
 
 from .contact_tracing import *
 from .constraint import *
@@ -14,15 +15,16 @@ from .solve import *
 from . import *
 
 
-def initial_shock(G: nx.graph, timesteps=5, p=0.1, num_shocks=7):
+SIR_TYPE = namedtuple("SIR_TYPE", ["S", "I_QUEUE", "R", "label"])
 
+def initial_shock(G: nx.graph, timesteps=5, p=0.1, num_shocks=7):
     full_data = EoN.basic_discrete_SIR(
         G=G, p=0.5, rho=.0001, tmin=0, tmax=1, return_full_data=True)
 
     for t in range(timesteps):
-        #shock_I =  set([i for i in range(n) if random.random() < 0.001])
+        # shock_I =  set([i for i in range(n) if random.random() < 0.001])
 
-        #S = set([k for (k,v) in full_data.get_statuses(time=1).items() if v == 'S']).difference(shock_I)
+        # S = set([k for (k,v) in full_data.get_statuses(time=1).items() if v == 'S']).difference(shock_I)
         S = set([k for (k, v) in full_data.get_statuses(
             time=1).items() if v == 'S'])
         I = set([k for (k, v) in full_data.get_statuses(
@@ -46,6 +48,7 @@ def initial_shock(G: nx.graph, timesteps=5, p=0.1, num_shocks=7):
     R = set([k for (k, v) in full_data.get_statuses(time=1).items() if v == 'R'])
 
     return (list(S), list(I), list(R))
+
 
 # returns the nodes in S, I, R after timesteps
 
@@ -104,7 +107,6 @@ def initial(G: nx.graph = None, timesteps=5, p=0.1, cache=None, from_cache=None)
 
 
 def MDP_step(G, S, I_t, R, Q1, Q2, p):
-
     full_data = EoN.basic_discrete_SIR(
         G=G, p=p, initial_infecteds=I_t, initial_recovereds=R + Q1 + Q2, tmin=0, tmax=1, return_full_data=True)
 
@@ -115,7 +117,8 @@ def MDP_step(G, S, I_t, R, Q1, Q2, p):
     return (S, I, R)
 
 
-def MDP(G: nx.graph, budget, S, I_t, R, p=0.5, iterations=10, method="dependent", visualization=False, verbose=False, **kwargs):
+def MDP(G: nx.graph, budget, S, I_t, R, p=0.5, iterations=10, method="dependent", visualization=False, verbose=False,
+        **kwargs):
     """
     Simulates a discrete step SIR model on graph G. Infected patients recover within one time period.
 
@@ -177,9 +180,11 @@ def MDP(G: nx.graph, budget, S, I_t, R, p=0.5, iterations=10, method="dependent"
             break
 
         if verbose:
-            print(str(t) + " " + str(len(I_t)) + " " + str(len(S)) + " " + str(len(R)))
+            print(str(t) + " " + str(len(I_t)) + " " +
+                  str(len(S)) + " " + str(len(R)))
 
-        (val, recommendation) = to_quarantine(G, I_t, R, budget, method=method, p=p)
+        (val, recommendation) = to_quarantine(
+            G, I_t, R, budget, method=method, p=p)
 
         (S, I_t, R) = MDP_step(G, S, I_t, R, Q_infected, Q_susceptible, p=p)
         # after this, R will contain Q_infected and Q_susceptible
@@ -228,3 +233,150 @@ def MDP(G: nx.graph, budget, S, I_t, R, p=0.5, iterations=10, method="dependent"
         plt.show()
 
     return (len(R), peak, total_iterated)
+
+
+def shock(S, I, num_shocks):
+    shocks = random.sample(S, num_shocks)
+
+    S = S.difference(shocks)
+    I = I.union(shocks)
+
+    return (S, I)
+
+
+def Generalized_MDP(G: nx.graph,
+                    p: float, # Required
+                    budget: int, # Required
+                    method: str, # Required
+                    MDP_iterations: int,
+                    num_shocks: int, # Required
+                    num_initial_infections: int,
+                    initial_iterations: int, # Data
+                    iterations_to_recover: int = 1, # Required
+                    cache: str = None, # Data
+                    from_cache: str = None,
+                    shock_MDP: bool = False, # Required
+                    visualization: bool = False, # Required
+                    verbose: bool = False): # Required
+    S = set()
+    I = set()
+    R = set()
+    infected_queue = []
+
+    # Data set up
+    if from_cache:
+        with open(PROJECT_ROOT / "data" / "SIR_Cache" / from_cache, 'r') as infile:
+            j = json.load(infile)
+            (S, infected_queue, R) = (
+                set(j["S"]), j["I_Queue"], set(j["R"]))
+
+            # Make infected_queue a list of sets
+            infected_queue = [set(s) for s in infected_queue]
+            I = I.union(*infected_queue)
+            if len(infected_queue) != iterations_to_recover:
+                raise ValueError(
+                    "Infected queue length must be equal to iterations_to_recover")
+
+    else:
+        # initialize S, I, R
+        I = set(random.sample(range(len(G.nodes)), num_initial_infections))
+        S = set([i for i in range(len(G.nodes))]).difference(I)
+        R = set()
+
+        # initialize the queue for recovery
+        infected_queue = [set() for _ in range(iterations_to_recover)]
+        infected_queue.pop(0)
+        infected_queue.append(I)
+
+        if verbose:
+            print(len(S), len(I), len(R))
+
+        for t in range(initial_iterations):
+
+            full_data = EoN.basic_discrete_SIR(
+                G=G, p=p, initial_infecteds=I, initial_recovereds=R, tmin=0, tmax=1, return_full_data=True)
+
+            # update susceptible, infected, and recovered sets
+            S = set([k for (k, v) in full_data.get_statuses(
+                time=1).items() if v == 'S'])
+            new_I = set([k for (k, v) in full_data.get_statuses(
+                time=1).items() if v == 'I'])
+
+            (S, new_I) = shock(S, new_I, num_shocks)
+
+            to_recover = infected_queue.pop(0)
+            infected_queue.append(new_I)
+
+            I = I.difference(to_recover)
+            I = I.union(new_I)
+            R = R.union(to_recover)
+
+            if verbose:
+                print(len(S), len(I), len(R))
+
+    if cache:
+        save = {
+            "S": list(S),
+            # convert list of sets into list of queue
+            "I_Queue": [list(s) for s in infected_queue],
+            "R": list(R),
+        }
+        with open(PROJECT_ROOT / "data" / "SIR_Cache" / cache, 'w') as outfile:
+            json.dump(save, outfile)
+
+    # Running the simulation
+    peak = 0
+    total_iterated = 0
+    Q_infected = []
+    Q_susceptible = []
+
+    if MDP_iterations == -1:
+        iterator = itertools.count(start=0, step=1)
+    else:
+        iterator = range(MDP_iterations)
+
+    print()
+
+    for t in iterator:
+
+        # Loop until no infected left.
+        if (MDP_iterations == -1) & (len(I) == 0):
+            total_iterated = t + initial_iterations
+            break
+
+        # get recommended quarantine
+        (val, recommendation) = to_quarantine(
+            G, I, R, budget, method=method, p=p)
+
+        # go through one step of the disease spread
+        # (S, I, R) = MDP_step(G, S, I, R, Q_infected, Q_susceptible, p=p)
+
+        full_data = EoN.basic_discrete_SIR(G=G, p=p, initial_infecteds=I, initial_recovereds=list(
+            R) + Q_infected + Q_susceptible, tmin=0, tmax=1, return_full_data=True)
+
+        S = set([k for (k, v) in full_data.get_statuses(
+            time=1).items() if v == 'S'])
+        new_I = set([k for (k, v) in full_data.get_statuses(
+            time=1).items() if v == 'I'])
+
+        if shock_MDP:
+            (S, new_I) = shock(S, new_I, num_shocks)
+
+        to_recover = infected_queue.pop(0)
+        infected_queue.append(new_I)
+
+        I = I.difference(to_recover)
+        I = I.union(new_I)
+        R = R.union(to_recover)
+
+        print(len(S), len(I), len(R))
+
+        # people are quarantined (removed from graph temporarily after the timestep)
+        for (k, v) in recommendation.items():
+            if v == 1:
+                if k in S:
+                    S.remove(k)
+                    Q_susceptible.append(k)
+                elif k in I:  # I_t is undefined
+                    I.remove(k)
+                    Q_infected.append(k)
