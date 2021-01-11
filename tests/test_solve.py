c927da19ecbf4c20a8211c23ccf6494b7be64178
Zach Zhao <thezachzhao@gmail.com>
2021-01-11 11:20:10 -0500

    Fix tests and weighted greedy

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -16,7 +16,12 @@ def test_degree_weighted_tree():
     K = 5
     _, degreeSol = to_quarantine(G=G, I0=I, safe=[], cost_constraint=K, runs=20, p=1, P=None, Q=None, method="degree")
     _, weightedSol = to_quarantine(G=G, I0=I, safe=[], cost_constraint=K, runs=20, p=1, P=None, Q=None, method="weighted")
-    assert set(degreeSol.keys()) == set(weightedSol.keys())
+
+    sol1 = {k for k,v in degreeSol.items() if v == 1}
+    sol2 = {k for k,v in weightedSol.items() if v == 1}
+
+    diffs = sol1 ^ sol2
+    assert len(diffs) < 0.05 * len(degreeSol)
 
 def test_degree_weighted_montgomery():
     # Setup montgomery graph
@@ -35,23 +40,15 @@ def test_degree_weighted_montgomery():
     time2 = end2 - end1
     print(f"Degree time: {time1}")
     print(f"Weighted time: {time2}")
-    assert set(degreeSol.keys()) == set(weightedSol.keys())
 
-def test_degree_weighted_equiv():
-    # Setup montgomery graph
-    G = load_graph("montgomery")
-    n = len(G.nodes)
-    I = [i for i in range(n) if random.random() > 0.99]
-    K = 50
+    # Collect all the 1 indicators
+    sol1 = {k for k,v in degreeSol.items() if v == 1}
+    sol2 = {k for k,v in weightedSol.items() if v == 1}
 
-    start = time.time()
-    _, degreeSol = to_quarantine(G=G, I0=I, safe=[], cost_constraint=K, runs=20, p=1, P=None, Q=None, method="weighted_expr")
-    end1 = time.time()
-    _, weightedSol = to_quarantine(G=G, I0=I, safe=[], cost_constraint=K, runs=20, p=1, P=None, Q=None, method="weighted")
-    end2 = time.time()
+    diffs = sol1 ^ sol2
+    print(f"Diffs: {diffs}")
 
-    time1 = end1 - start
-    time2 = end2 - end1
-    print(f"Weighted_Expr time: {time1}")
-    print(f"Weighted time: {time2}")
-    assert set(degreeSol.keys()) == set(weightedSol.keys())
+    # Differ by at most 5% of the set
+    tolerance = 0.05 * len(sol1)
+    assert len(diffs) < tolerance
+    assert False
