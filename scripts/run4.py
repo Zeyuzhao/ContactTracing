62cafa9234f578f190070a7154df82e8056e5238
Ubuntu <ubuntu@ip-172-31-95-153.ec2.internal>
2021-01-13 16:37:52 +0000

    visualizations

--- scripts/run4.py
+++ scripts/run4.py
@@ -90,15 +90,15 @@ for line in lines:
 # Experiment 1
 COMPACT_CONFIG = {
     "G": [G2], # Graph
-    "p": [0.06], # Probability of infection
-    "budget": [i for i in range(500,2001,10)], # The k value
+    "p": [0.09], # Probability of infection
+    "budget": [i for i in range(500,2001,50)], # The k value
     "method": ["random","degree","dependent","weighted"],
-    "num_initial_infections": [3], # Initial Initial (DATA)
-    "num_shocks": [20], # Size of shocks in initial (DATA)
-    "initial_iterations": [1], # Number of iterations before intervention
+    "num_initial_infections": [5], # Initial Initial (DATA)
+    "num_shocks": [5], # Size of shocks in initial (DATA)
+    "initial_iterations": [5], # Number of iterations before intervention
     "MDP_iterations": [-1], # Number of iterations of intervention
     "iterations_to_recover": [1], # Number of iterations it takes for a infected node to recover (set to 1)
-    "from_cache": ['a5.json'], # If cache is specified, some arguments are ignored
+    "from_cache": [None], # If cache is specified, some arguments are ignored
     "verbose": [False], # Prints stuff
     "trials": 10, # Number of trials to run for each config
 }
