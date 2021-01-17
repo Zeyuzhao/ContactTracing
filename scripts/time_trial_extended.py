3d32b10074e9b843158707f4957ef6cbf21e43bb
Zeyuzhao <thezachzhao@gmail.com>
2021-01-17 19:27:33 +0000

    Fix script to load graphs

--- scripts/time_trial_extended.py
+++ scripts/time_trial_extended.py
@@ -2,7 +2,7 @@ from ctrace.experiments import time_trial_extended_tracker
 from ctrace.runner import *
 from ctrace.simulation import generalized_mdp
 from ctrace.experiments import *
-
+from ctrace.dataset import load_graph
 json_dir = PROJECT_ROOT / "data" / "SIR_Cache" / "time_trials"
 
 cache_paths = [f for f in json_dir.iterdir()][:300] # Taking the first 300 files
@@ -15,10 +15,11 @@ config = {
     "method": ["dependent", "weighted"],
     "from_cache": cache_paths, # If cache is specified, some arguments are ignored
 }
+config["G"] = [load_graph(x) for x in config["G"]]
 
 # in_schema and out_schema MUST match the input arguments and namedtuple respectively!
 in_schema = list(config.keys())
 out_schema = list(TimeTrialExtendTrackerInfo._fields)
-run = GridExecutorParallel.init_multiple(config, in_schema, out_schema, func=time_trial_extended_tracker, trials=2)
+run = GridExecutorLinear.init_multiple(config, in_schema, out_schema, func=time_trial_extended_tracker, trials=2)
 # Attempt at making schemas extensible - quite hacky right now
 run.exec()
