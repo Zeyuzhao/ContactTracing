a36e8ab7e9a645b94837a62568b1543bf8938f2f
gzli929 <gzli929@gmail.com>
2021-01-24 10:06:55 -0500

    moved stuff to recommende.py

--- ctrace/recommender.py
+++ ctrace/recommender.py
@@ -1,9 +1,60 @@
+import random
 import numpy as np
 
 from .problem import *
 
+def random(V_1, cost_constraint):
+    sample = random.sample(V_1, min(cost_constraint, len(V_1)))
+    sol = {}
+    for v in V_1:
+        if v in sample:
+            sol[v] = 1
+        else:
+            sol[v] = 0
+    return (-1, sol)
+
+def degree(G, V_1, V_2, cost_constraint):
+    # calculate degree of each vertex in V_1
+    degrees = []
+    for u in V_1:
+        count = 0
+        for v in set(G.neighbors(u)):
+            if v in V_2:
+                count += 1
+
+        degrees.append((count, u))
+    degrees.sort()
+    degrees.reverse()
+    sol = {}
+    for i in range(len(V_1)):
+        if i < cost_constraint:
+            sol[degrees[i][1]] = 1
+        else:
+            sol[degrees[i][1]] = 0
+    return (-1, sol)
+
+def weighted(G, I0, P, Q, V_1, V_2, cost_constraint, costs):
+    weights: List[Tuple[int, int]] = []
+    for u in V_1:
+        w_sum = 0
+        for v in set(G.neighbors(u)):
+            if v in V_2:
+                w_sum += Q[u][v]
+        weights.append((P[u] * w_sum, u))
+    # Get the top k (cost_constraint) V1s ranked by w_u = p_u * sum(q_uv for v in v2)
+    weights.sort(reverse=True)
+    topK = weights[:cost_constraint]
+    topK = {i[1] for i in topK}
+    sol = {}
+    for u in V_1:
+        if u in topK:
+            sol[u] = 1
+        else:
+            sol[u] = 0
+    return -1, sol
+
 #returns rounded bits and objective value of those bits
-def basic_non_integer_round(problem: MinExposedLP):
+def dependent(problem: MinExposedLP):
     problem.solve_lp()
     probabilities = problem.get_variables()
     rounded = D_prime(np.array(probabilities))
@@ -17,7 +68,7 @@ def basic_non_integer_round(problem: MinExposedLP):
     return (problem.objective_value, problem.quarantined_solution)
 
 #returns rounded bits and objective value of those bits
-def iterated_round(problem: ProbMinExposed, d: int):
+def iterated(problem: ProbMinExposed, d: int):
     problem.solve_lp()
     probabilities = np.array(problem.get_variables())
     
@@ -48,7 +99,7 @@ def iterated_round(problem: ProbMinExposed, d: int):
     return (problem.objective_value, problem.quarantined_solution)
 
 #returns rounded bits and objective value of those bits
-def optimized_iterated_round(problem: ProbMinExposed, d: int):
+def optimized(problem: ProbMinExposed, d: int):
     problem.solve_lp()
     probabilities = np.array(problem.get_variables())
     
