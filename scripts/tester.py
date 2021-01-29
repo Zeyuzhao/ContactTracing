e4bea5a9506da5a31c222432c51518b4ea908dad
Zach Zhao <thezachzhao@gmail.com>
2021-01-29 00:36:37 -0500

    Working starter code
    Former-commit-id: 30ade29674f5df725fc27b29c3fc86cd7d92d661

---
+++ scripts/tester.py
@@ -0,0 +1,5 @@
+from ctrace.simulation import SimulationState, InfectionInfo
+from ctrace.utils import *
+from ctrace.dataset import *
+from ctrace.problem import MinExposedIP, MinExposedLP, dependent
+
