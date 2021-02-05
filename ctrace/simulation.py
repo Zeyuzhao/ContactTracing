6d32994a0da33dde5ceb291dd503b0264a2ae486
Zach Zhao <thezachzhao@gmail.com>
2021-02-05 15:20:42 -0500

    Implement draft

--- ctrace/simulation.py
+++ ctrace/simulation.py
@@ -1,3 +1,4 @@
+#%%
 import json
 import random
 from enum import Enum, IntEnum
@@ -6,8 +7,8 @@ import EoN
 import networkx as nx
 import numpy as np
 
-from typing import Set, List
-from collections import namedtuple, defaultdict
+from typing import Dict, Set, List, Any, TypeVar
+from collections import UserList, namedtuple, defaultdict
 
 # from .utils import find_excluded_contours
 from ctrace import PROJECT_ROOT
@@ -20,10 +21,59 @@ SIR_Tuple = namedtuple("SIR_Tuple", ["S", "I", "R"])
 
 # %%
 
-class SIR(IntEnum):
+# Declare SIR Enum
+class SIR:
     S = 0
     I = 1
     R = 2
+
+
+#%%
+
+from collections import UserList
+# TODO: Add testing?
+
+T = TypeVar('T', bound='PartitionSIR')
+class PartitionSIR(UserList):
+    def __init__(self, size=0):
+        # Stored internally as integers
+        self._types = ["S", "I", "R"]
+        self.type = IntEnum("type", zip(self._types, range(3)))
+        self.data = [0] * size
+
+    @classmethod
+    def from_list(cls, l):
+        p = PartitionSIR()
+        p.data = l.copy()
+        return p
+    
+    @classmethod
+    def from_dict_letters(cls, n: int, d: Dict[int, str]) -> T:
+        mapper = {
+            "S": 0,
+            "I": 1,
+            "R": 2,
+        }
+        p = PartitionSIR(n)
+        for k, v in d.items():
+            p[k] = mapper[v]
+
+    def __getitem__(self, item: int) -> int:
+        return self.data[item]
+    
+    def __setitem__(self, key: int, value: int) -> None:
+        self.data[key] = value
+
+    # TODO: Properties hardcoded - hacky solution
+    @property
+    def S(self):
+        return (i for i, e in enumerate(self.data) if e == 0)
+    @property
+    def I(self):
+        return (i for i, e in enumerate(self.data) if e == 1)
+    @property
+    def R(self):
+        return (i for i, e in enumerate(self.data) if e == 2)
 
 
 # %%
@@ -49,7 +99,8 @@ class InfectionEnv(gym.Env):
         })
 
         # State information
-        self.SIR_History = None
+        self.SIR_History = []
+        self.quarantine_history = []
         self.time_step = 0
 
         # Initialization parameters
@@ -65,6 +116,8 @@ class InfectionEnv(gym.Env):
 
         # The state of infection over all time steps
         self.SIR_History = []
+        self.quarantine_history = []
+        self.time_step = 0
 
         # Generate the initial clusters
         obs = np.zeros(self.N)
@@ -74,50 +127,69 @@ class InfectionEnv(gym.Env):
 
         # Give the infection a head start
         self.time_step = 0
+        no_op = [0] * self.N
         for i in range(self.delay):
-            obs, _, _, _ = self.step([0] * self.N) # Let infection advance ahead delay steps
+            self._step(no_op) # Let infection advance ahead delay steps
         return obs
 
     def step(self, action: List[int]):
-        # Retrieve current state.
-        state = self.SIR_History[-1].copy()
-        q = [i for i in action if action[i] == 1]
+        self._step(action)
+        # Retrieve SIR that is stale
+        obs = {
+            'sir': self.SIR_History[-(self.stale + 1)],
+            'contours': [0] * self.N,  # Contours are ignored
+            'time': self.time_step - self.delay,
+        }
 
-        # Quarantine
-        quarantine_dict = {i: state[i] for i in q}
+        I_count = obs['sir'].count(SIR.I)
+        reward = I_count  # Number of people in infected
+
+        # TODO: Test done condition!!!
+        done = (I_count == 0) or self.time_step > self.total_time  
+        # Info for tracking progress of simulation
+        info = {}
+        return obs, reward, done, info
+    
+    def _step(self, action: List[int]) -> None:
+        # Retrieve current state.
+        partition = PartitionSIR.from_list(self.SIR_History[-1])
+
+        # Action -> a integer boolean array of quarantine members
+        # Map quarantine members to its state
+        quarantine_dict = {i: partition[i] for i in action if action[i] == 1}
+
+        # Move members into R (temporarily)
+        for q in quarantine_dict: 
+            partition[q] = SIR.R  # 2
 
         # Run simulation
         full_data = EoN.basic_discrete_SIR(
             G=self.G,
             p=self.transmission_rate,
-            initial_infecteds=sir[SIR.I],
-            initial_recovereds=sir[SIR.R],
+            initial_infecteds=partition.I,
+            initial_recovereds=partition.R,
             tmin=0,
             tmax=1,
             return_full_data=True
         )
-        # TODO: Unquarantine Q dict -> SIR
-        # TODO: Store results into SIR_History
+        # Advance quarantined
+        for q, status in quarantine_dict.items():
+            # I -> R
+            if status == SIR.I:
+                quarantine_dict[q] = SIR.R
+            # S -> S (nothing)
+        
+        result_partition = PartitionSIR.from_dict_letters(self.N, 
+        full_data.get_statuses(time=1))
 
+        # Move quarantine back into graph (undo the R state)
+        for q, status, in quarantine_dict.items():
+            result_partition[q] = status
+        
+        # Store results into SIR_History
+        self.SIR_History.append(result_partition.data)
         self.time_step += 1
 
-        # Retrieve SIR that is stale
-        obs = {
-            'sir': self.SIR_History[-(self.stale + 1)],
-            'contours': [0] * self.N  # Contours are ignored
-        }
-
-        reward = 0  # Difference in infected people?
-        done = False  # If time steps reached or number infected?
-        # Info for tracking progress of simulation
-        info = {}
-        return obs, reward, done, info
-
-    def _quarantine(self):
-        raise NotImplementedError
-
-    def _unquarantine(self):
-        raise NotImplementedError
 
     def seed(self, seed=None):
         # Set seeds???
@@ -142,10 +214,6 @@ class PartitionSet(List):
     def __setitem__(self, key, value):
         raise NotImplementedError
 
-class Partition(List):
-    def __init__(self, attrs, size=0):
-        # Stored internally as integers
-        pass
 
 # %%
 # TODO: Add testing
