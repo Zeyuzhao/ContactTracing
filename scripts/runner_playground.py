28cca8a7b1a4c29278c5dd4b5912ba1ad2063e18
Ubuntu <ubuntu@ip-172-31-95-153.ec2.internal>
2021-02-07 17:35:01 +0000

    changed runner

--- scripts/runner_playground.py
+++ scripts/runner_playground.py
@@ -9,18 +9,18 @@ from collections import namedtuple
 json_dir = PROJECT_ROOT / "data" / "SIR_Cache"
 
 #Take only files that start with t, YES this is a terrible solution. Also, I only took 1 file.
-cache_paths = [f for f in json_dir.iterdir() if str(f)[str(f).rindex("\\")+1]=="t"][:1] # Taking the first 400 files
+cache_paths = [f for f in json_dir.iterdir() if str(f)[str(f).rindex("/")+1]=="t"][:1] # Taking the first 400 files
 
 print(f"Number of json files: {len(cache_paths)}")
 
 config = {
     "G" : ["montgomery"],
-    "budget": [i for i in range(100, 1000, 50)],
+    "budget": [1000],
     "transmission_rate": [0.078],
-    "compliance_rate": [i/100 for i in range(30, 100, 5)],
-    "global_rate":  [i/100 for i in range(5, 100, 5)],         #ranges are not currently inclusive of upper bound
-    "discovery_rate": [i/100 for i in range(5, 100, 5)],
-    "snitch_rate":  [i/100 for i in range(5, 100, 5)],
+    "compliance_rate": [i/100 for i in range(10, 101, 1)],
+    "global_rate":  [1],         #ranges are not currently inclusive of upper bound
+    "discovery_rate": [1],
+    "snitch_rate":  [1],
     #QUESTION: For taking the cartesian product, would unnecessary computation happen since snitch+discovery rates should be the same?
     "from_cache": cache_paths,
     "agent_name": ["Degree"]
@@ -66,9 +66,28 @@ def time_trial_tracker(G: nx.graph, budget: int, transmission_rate: float, compl
     return TrackerInfo(len(state.SIR_known.SIR[2]), len(state.SIR_real.SIR[2]))
 
 #My laptop is weird and parallel doesn't work for me. Switch back to Parallel to actually test it.
-run = GridExecutorLinear.init_multiple(config, in_schema, out_schema, func=time_trial_tracker, trials=1)
+run = GridExecutorParallel.init_multiple(config, in_schema, out_schema, func=time_trial_tracker, trials=10)
 # Attempt at making schemas extensible - quite hacky right now
 # run.track_duration()
+#run.exec()
+
+
+config = {
+    "G" : ["montgomery"],
+    "budget": [i for i in range(100,1001,10)],
+    "transmission_rate": [0.078],
+    "compliance_rate": [1],
+    "global_rate":  [1],         #ranges are not currently inclusive of upper bound
+    "discovery_rate": [1],
+    "snitch_rate":  [1],
+    #QUESTION: For taking the cartesian product, would unnecessary computation happen since snitch+discovery rates should be the same?
+    "from_cache": cache_paths,
+    "agent_name": ["Degree"]
+}
+config["G"] = [load_graph(x) for x in config["G"]]
+
+run = GridExecutorParallel.init_multiple(config, in_schema, out_schema, func=time_trial_tracker, trials=10)
+run.exec()
+
 
 #%%
-run.exec()
