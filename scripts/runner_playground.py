d29d4f0777a78393c9f18bc19f2ffa9821c88c02
al7gc@virginia.edu <al7gc@virginia.edu>
2021-03-13 00:12:12 -0500

    information loss progress

--- scripts/runner_playground.py
+++ scripts/runner_playground.py
@@ -66,11 +66,11 @@ G2 = readData();
 
 config = {
     "G" : [G2],
-    "budget": [i for i in range(100,3710,10)],
-    "transmission_rate": [0.06],
-    "compliance_rate": [i/100 for i in range(50,101,5)],
-    "global_rate":  [1],        
-    "discovery_rate": [1],
+    "budget": [1000, 2000],#[i for i in range(100,3710,10)],
+    "transmission_rate": [0.078],
+    "compliance_rate": [1],#[i/100 for i in range(50,101,5)],
+    "global_rate":  [0],        
+    "discovery_rate": [i/100 for i in range(5, 105, 5)],
     "snitch_rate":  [1],
     "from_cache": ["a6.json"],
     "agent": [DegGreedy]
@@ -78,7 +78,7 @@ config = {
 #config["G"] = [load_graph(x) for x in config["G"]]
 
 in_schema = list(config.keys())
-out_schema = ["infected_count_known", "infected_count_real"]
+out_schema = ["infected_count_known", "infected_count_real", "information_loss_V1", "information_loss_V2", "information_loss_I", "information_loss_V1_iterative", "information_loss_V2_iterative", "information_loss_V2_nbrs_iterative"]
 TrackerInfo = namedtuple("TrackerInfo", out_schema)
 
 def time_trial_tracker(G: nx.graph, budget: int, transmission_rate: float, compliance_rate: float, global_rate: float,
@@ -97,13 +97,34 @@ def time_trial_tracker(G: nx.graph, budget: int, transmission_rate: float, compl
 
     state = SimulationState(G, (S, I, R), (S, I, R), budget, transmission_rate, compliance_rate, global_rate, discovery_rate, snitch_rate)
     
+    information_loss_V1 = 0
+    information_loss_V2 = 0
+    information_loss_I = 0
+    information_loss_V1_iterative = 0
+    information_loss_V2_iterative = 0
+    information_loss_V2_nbrs_iterative = 0
+    
     while len(state.SIR_real.SIR[1]) != 0:
         to_quarantine = agent(state)
         state.step(to_quarantine)
+        
+        V1_real = state.SIR_real.V1
+        V1_known = state.SIR_known.V1
+        V2_real = state.SIR_real.V2
+        V2_known = state.SIR_known.V2
+        I_real = set(state.SIR_real.SIR.I)
+        I_known = set(state.SIR_known.SIR.I)
 
-    return TrackerInfo(len(state.SIR_known.SIR[2]), len(state.SIR_real.SIR[2]))
+        information_loss_V1 += len(V1_real-V1_known)
+        information_loss_V2 += len(V2_real-V2_known)
+        information_loss_I += len(I_real-I_known)
+        information_loss_V1_iterative += state.SIR_known.il_v1
+        information_loss_V2_iterative += state.SIR_known.il_v2
+        information_loss_V2_nbrs_iterative += state.SIR_known.il_v2_nbrs
 
-run = GridExecutorParallel.init_multiple(config, in_schema, out_schema, func=time_trial_tracker, trials=10)
+    return TrackerInfo(len(state.SIR_known.SIR[2]), len(state.SIR_real.SIR[2]), information_loss_V1, information_loss_V2, information_loss_I, information_loss_V1_iterative, information_loss_V2_iterative, information_loss_V2_nbrs_iterative)
+
+run = GridExecutorLinear.init_multiple(config, in_schema, out_schema, func=time_trial_tracker, trials=10)
 # Attempt at making schemas extensible - quite hacky right now
 # run.track_duration()
 run.exec()
