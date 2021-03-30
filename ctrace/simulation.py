e6e21d107abe582725cbec0080fe5f5694286eeb
Ubuntu <ubuntu@ip-172-31-95-153.ec2.internal>
2021-03-30 18:02:06 +0000

    this better be final

--- ctrace/simulation.py
+++ ctrace/simulation.py
@@ -11,123 +11,66 @@ from collections import namedtuple
 from .utils import find_excluded_contours
 from . import PROJECT_ROOT
 
-SIR_Tuple = namedtuple("SIR_Tuple", ["S", "I", "R"])
-
-class SimulationState:
+SIR_Tuple = namedtuple("SIR_Tuple", ["S", "I1", "I2", "R"])
+                
+class InfectionInfo:
     
-    def __init__(self, G:nx.graph, SIR_real: SIR_Tuple, SIR_known: SIR_Tuple, budget: int, transmission_rate:tuple, partition: tuple, time_stage: int, compliance_rate:float, global_rate:float, discovery_rate:float, snitch_rate:float):
+    def __init__(self, G:nx.graph, SIR: SIR_Tuple, budget:int, transmission_rate:float, compliance_rate:float = 1, I_knowledge:float = 1, discovery_rate:float = 1, snitch_rate:float = 1):
         self.G = G
-        self.SIR_real: InfectionInfo = InfectionInfo(G, SIR_real, budget, transmission_rate, partition, time_stage, 1, 1)
-        self.SIR_known: InfectionInfo = InfectionInfo(G, SIR_known, budget, transmission_rate, partition, time_stage, discovery_rate, snitch_rate)
+        self.SIR = SIR_Tuple(*SIR)
+        self.budget = budget
+        self.transmission_rate = transmission_rate
         self.compliance_rate = compliance_rate
-        self.global_rate = global_rate
+        self.discovery_rate = discovery_rate
+        self.snitch_rate = snitch_rate
+        self.quarantined = ([],[],[])
+        
+        # initialize V1 and V2
+        self.set_contours()
         
     # returns a SimulationState object loaded from a file
-    '''def load(self, G:nx.graph, file):
+    def load(self, G:nx.graph, file):
         with open(PROJECT_ROOT / "data" / "SIR_Cache" / file, 'r') as infile:
             j = json.load(infile)
             
             if G.name != j['G']:
                 raise Exception("Graph and SIR not compatible")
                 
-            SIR_real = (j["S_real"], j["I_real"], j["R_real"])
-            SIR_known = (j["S_known"], j["I_known"], j["R_known"])
+            SIR_real = (j["S"], j["I"], j["R"])
             
-            return SimulationState(G, SIR_real, SIR_known, j["transmission_rate"], j["compliance_rate"], j["global_rate"])'''
+            return SimulationState(G, SIR_real, j['budget'], j["transmission_rate"], j["compliance_rate"], j['I_knowledge'], j['discovery_rate'], j["snitch_rate"])
     
     # saves a SimulationState object to a file
     def save(self, file):
         
         to_save = {
-            "G": self.SIR_real.G.name,
-            "S_real": self.SIR_real[0],
-            "I_real": self.SIR_real[1],
-            "R_real": self.SIR_real[2],
-            "S_known": self.SIR_known[0],
-            "I_known": self.SIR_known[1],
-            "R_known": self.SIR_known[2],
-            "budget": self.SIR_real.budget,
-            "transmission_rate": self.SIR_real.transmission_rate,
+            "G": self.G.name,
+            "S": self.SIR[0],
+            "I": self.SIR[1],
+            "R": self.SIR[2],
+            "budget": self.budget,
+            "I_knowledge": self.I_knowledge,
+            "transmission_rate": self.transmission_rate,
             "compliance_rate": self.compliance_rate,
-            "global_rate": self.global_rate
+            "discovery_rate": self.discovery_rate,
+            "snitch_rate": self.snitch_rate
         }
         
         with open(PROJECT_ROOT / "data" / "SIR_Cache" / file, 'w') as outfile:
             json.dump(to_save, outfile)
 
-    # TODO: Adapt to indicators over entire Graph G
-    def step(self, quarantine_known: Set[int]):
-        
-        recovered_prev = len(self.SIR_real.SIR[2])
-        
-        # moves the real SIR forward by 1 timestep
-        recovered = self.SIR_real.SIR[2] + self.SIR_real.quarantined[0] + self.SIR_real.quarantined[1] + self.SIR_real.quarantined[2]
-        full_data = EoN.basic_discrete_SIR(G=self.G, p=self.SIR_real.transmission_rate[self.SIR_real.time_stage], initial_infecteds=self.SIR_real.SIR[1], initial_recovereds=recovered, tmin=0, tmax=1, return_full_data=True)
+
+    def step(self, quarantine: Set[int]):
+        # moves the SIR forward by 1 timestep
+        recovered = self.SIR[2] + self.quarantined[0] + self.quarantined[1] + self.quarantined[2]
+        full_data = EoN.basic_discrete_SIR(G=self.G, p=self.transmission_rate, initial_infecteds=self.SIR_real.SIR[1], initial_recovereds=recovered, tmin=0, tmax=1, return_full_data=True)
         
         S = [k for (k, v) in full_data.get_statuses(time=1).items() if v == 'S']
         I = [k for (k, v) in full_data.get_statuses(time=1).items() if v == 'I']
         R = [k for (k, v) in full_data.get_statuses(time=1).items() if v == 'R']
         
-        self.SIR_real.SIR = SIR_Tuple(S,I,R)
-        
-        #Transition into the late phase: Can jump straight into late phase and skip peak
-        if(self.SIR_real.time_stage != 2 and len(self.SIR_real.SIR[1])/max(1,len(self.SIR_real.SIR[2])) < self.SIR_real.partition[1]):
-            self.SIR_real.time_stage = 2
-        #Transition into the middle phase
-        elif(self.SIR_real.time_stage == 0 and len(self.SIR_real.SIR[2])/len(self.G.nodes) > self.SIR_real.partition[self.SIR_real.time_stage]):
-            self.SIR_real.time_stage+=1
-        
-        # moves the known SIR forward by 1 timestep
-        I = [i for i in self.SIR_known.V1 if i in self.SIR_real.SIR.I]
-        S = [i for i in self.SIR_known.SIR.S if i not in I]
-        R = self.SIR_known.SIR.R + self.SIR_known.SIR.I + self.SIR_known.quarantined[0] + self.SIR_known.quarantined[1] + self.SIR_known.quarantined[2]
-        
-        self.SIR_known.SIR = SIR_Tuple(S,I,R)
-        
-        #Transition into the late phase: Can jump straight into late phase and skip peak
-        if(self.SIR_known.time_stage != 2 and len(self.SIR_known.SIR[1])/max(1,len(self.SIR_known.SIR[2])) < self.SIR_known.partition[1]):
-            self.SIR_known.time_stage = 2
-        #Transition into the middle phase
-        elif(self.SIR_known.time_stage == 0 and len(self.SIR_known.SIR[2])/len(self.G.nodes) > self.SIR_known.partition[self.SIR_known.time_stage]):
-            self.SIR_known.time_stage+=1
-        
-        # implements the global rate
-        difference = [i for i in self.SIR_real.SIR.I if i not in self.SIR_known.SIR.I]
-        for node in difference:
-            if random.uniform(0,1) <= self.global_rate:
-                if node in self.SIR_known.SIR.S:
-                    self.SIR_known.SIR.I.append(node)
-                    self.SIR_known.SIR.S.remove(node)
-                else:
-                    self.SIR_known.quarantined[1].append(node)
-                    self.SIR_known.quarantined[0].remove(node)
-        
-        
-        # updates the quarantined people
-        quarantine_real = {i for i in quarantine_known if random.random() < self.compliance_rate}
-        self.SIR_real.update_quarantine(quarantine_real)
-        self.SIR_known.update_quarantine(quarantine_known)
-        
-        
-        # resets the V1 and V2
-        self.SIR_known.set_contours()
-        self.SIR_real.set_contours()
-                
-class InfectionInfo:
-    
-    def __init__(self, G:nx.graph, SIR: SIR_Tuple, budget:int, transmission_rate:tuple, partition: tuple, time_stage: int, discovery_rate:tuple, snitch_rate:float):
-        self.G = G
-        self.SIR = SIR_Tuple(*SIR)
-        self.transmission_rate = transmission_rate
-        self.time_stage = time_stage
-        self.partition = partition
-        self.budget = budget
-        self.quarantined = ([],[],[])
-        self.discovery_rate = discovery_rate
-        self.snitch_rate = snitch_rate
-        
-        # initialize V1 and V2
-        self.set_contours()
+        to_quarantine = [u for u in quarantine if
+        update_quarantine(to_quarantine
         
     def update_quarantine(self, to_quarantine):
         # un-quarantine people from previous time step
