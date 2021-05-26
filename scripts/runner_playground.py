41fde0cb2bc01db1579cc57255c2a787f8979db2
Ann924 <al7gc@virginia.edu>
2021-05-26 10:17:02 -0400

    hi george again again

--- scripts/runner_playground.py
+++ scripts/runner_playground.py
@@ -9,11 +9,11 @@ from ctrace.recommender import *
 from collections import namedtuple
 json_dir = PROJECT_ROOT / "data" / "SIR_Cache"
 
-#G = load_graph_montgomery_labels()
+G = load_graph_montgomery_labels()
 #G = read_extra_edges(G, 0.15)
-#G.centrality = nx.algorithms.eigenvector_centrality_numpy(G)
+G.centrality = nx.algorithms.eigenvector_centrality_numpy(G)
 
-G2 = load_graph_cville_labels()
+#G2 = load_graph_cville_labels()
 #G2 = read_extra_edges(G2, 0.15)
 #G2.centrality = nx.algorithms.eigenvector_centrality_numpy(G2)
 
@@ -28,17 +28,16 @@ G2 = load_graph_cville_labels()
 #cville budget: 1350
 
 config = {
-    "G" : [G2],
-    "budget": [i for i in range(720, 2270, 20)],
+    "G" : [G],
+    "budget": [750],
     "policy": ["none"],
     "transmission_rate": [0.05],
     "transmission_known": [True],
-    "compliance_rate": [-1],
+    "compliance_rate": [0.8],
     "compliance_known": [True],
-    "discovery_rate": [1],
-    "snitch_rate": [1],
-    "from_cache": ["b5.json"],
-    "agent": [Degree2]
+    "snitch_rate": [i/100 for i in range(50, 101, 1)],
+    "from_cache": ["c7.json"],
+    "agent": [Random, EC]
 }
 
 
@@ -46,7 +45,7 @@ in_schema = list(config.keys())
 out_schema = ["infection_count", "infections_step"]
 TrackerInfo = namedtuple("TrackerInfo", out_schema)
 
-def time_trial_tracker(G: nx.graph, budget: int, policy:str, transmission_rate: float, transmission_known: bool, compliance_rate: float, compliance_known:bool, discovery_rate: float, snitch_rate: float, from_cache: str, agent, **kwargs):
+def time_trial_tracker(G: nx.graph, budget: int, policy:str, transmission_rate: float, transmission_known: bool, compliance_rate: float, compliance_known:bool, snitch_rate: float, from_cache: str, agent, **kwargs):
 
     with open(PROJECT_ROOT / "data" / "SIR_Cache" / from_cache, 'r') as infile:
             j = json.load(infile)
@@ -54,7 +53,7 @@ def time_trial_tracker(G: nx.graph, budget: int, policy:str, transmission_rate: 
             (S, I1, I2, R) = (j["S"], j["I1"], j["I2"], j["R"])
             infections = j["infections"]
 
-    state = InfectionState(G, (S, I1, I2, R), budget, policy, transmission_rate, transmission_known, compliance_rate, compliance_known, discovery_rate, snitch_rate)
+    state = InfectionState(G, (S, I1, I2, R), budget, policy, transmission_rate, transmission_known, compliance_rate, compliance_known, snitch_rate)
     
     while len(state.SIR.I1) + len(state.SIR.I2) != 0:
         to_quarantine = agent(state)
@@ -64,8 +63,6 @@ def time_trial_tracker(G: nx.graph, budget: int, policy:str, transmission_rate: 
     return TrackerInfo(len(state.SIR.R), infections)
 
 
-run = GridExecutorParallel.init_multiple(config, in_schema, out_schema, func=time_trial_tracker, trials=10)
+run = GridExecutorParallel.init_multiple(config, in_schema, out_schema, func=time_trial_tracker, trials=20)
 run.exec()
-
-
 #%%
